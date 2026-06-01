0e04bf7ab06c64b4b7a98e9e003e304a2d381771
Fiantso Harena <fiantsorav@gmail.com>
2026-06-01 17:53:53 +0300

    feat(product-service): add product update endpoint and route

--- apps/product-service/routes/product.route.ts
+++ apps/product-service/routes/product.route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
-import { createProduct } from '../controllers/product.controller';
+import { createProduct, updateProduct } from '../controllers/product.controller';
+import { shouldBeAdmin } from '../middleware/auth.middleware';
 
 const router: Router = Router();
 
 router.post('/', createProduct);
+router.put('/:id', shouldBeAdmin, updateProduct);
 
 export default router;
